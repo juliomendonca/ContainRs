dfbbcfef0bab0700027d2142ff0d4d5281d4f44b
Júlio Cézar Mendonça <juliomendonca@outlook.com>
2025-05-09 14:09:25 -0300

    Add project files.

---
+++ src/ContainRs.Api/Locacoes/LocacoesEndpoints.cs
@@ -0,0 +1,43 @@
+﻿using ContainRs.Api.Contracts;
+using ContainRs.Api.Domain;
+using ContainRs.Api.Endpoints;
+using ContainRs.Api.Extensions;
+using ContainRs.Api.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContainRs.Api.Locacoes;
+
+public static class LocacoesEndpoints
+{
+    public static IEndpointRouteBuilder MapLocacoesEndpoints(this IEndpointRouteBuilder builder)
+    {
+        var group = builder
+            .MapGroup(EndpointConstants.ROUTE_LOCACOES)
+            .RequireAuthorization(builder => builder.RequireRole("Cliente"))
+            .WithTags(EndpointConstants.TAG_LOCACAO)
+            .WithOpenApi();
+
+        group
+            .MapGetLocacoes();
+
+        return builder;
+    }
+
+    public static RouteGroupBuilder MapGetLocacoes(this RouteGroupBuilder builder)
+    {
+        builder.MapGet("", async (
+            HttpContext context,
+            [FromServices] IRepository<Locacao> repository) =>
+        {
+            var clienteId = context.GetClienteId();
+            if (clienteId is null) return Results.Unauthorized();
+
+            var locacoes = await repository
+                .GetWhereAsync(l => l.ClienteId == clienteId.Value);
+            return Results.Ok(locacoes.Select(LocacaoResponse.From));
+        })
+        .WithSummary("Lista o histórico de locações do cliente");
+
+        return builder;
+    }
+}
---
+++ src/ContainRs.Api/Program.cs
@@ -0,0 +1,69 @@
+using ContainRs.Api.Clientes;
+using ContainRs.Api.Containeres;
+using ContainRs.Api.Contracts;
+using ContainRs.Api.Data;
+using ContainRs.Api.Data.Repositories;
+using ContainRs.Api.Domain;
+using ContainRs.Api.Identity;
+using ContainRs.Api.Locacoes;
+using ContainRs.Api.Propostas;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<IdentityDbContext>(options =>
+{
+    options
+        .UseSqlServer(builder.Configuration.GetConnectionString("IdentityDB"));
+});
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options
+        .UseSqlServer(builder.Configuration.GetConnectionString("ContainRsDB"));
+});
+
+builder.Services.AddScoped<IRepository<Cliente>, ClienteRepository>();
+builder.Services.AddScoped<IRepository<Solicitacao>, SolicitacaoRepository>();
+builder.Services.AddScoped<IRepository<Proposta>, PropostaRepository>();
+builder.Services.AddScoped<IRepository<Locacao>, LocacaoRepository>();
+builder.Services.AddScoped<IRepository<Conteiner>, ConteinerRepository>();
+
+builder.Services
+    .AddIdentityApiEndpoints<AppUser>(options => options.SignIn.RequireConfirmedEmail = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<IdentityDbContext>();
+
+builder.Services.AddAuthorization();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.ClaimsIdentity.UserIdClaimType = "ClienteId";
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app
+    .MapIdentityEndpoints()
+    .MapClientesEndpoints()
+    .MapAprovacaoClientesEndpoints()
+    .MapSolicitacoesEndpoints()
+    .MapPropostasEndpoints()
+    .MapLocacoesEndpoints()
+    .MapConteineresEndpoints();
+
+app.Run();
